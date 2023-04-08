71811138af5189bb7dc38c091ee6679356b2e04a
refilwe204 <nqatyelwaangelin@gmail.com>
2023-04-08 10:18:33 +0200

    final commit

---
+++ scripts.js
@@ -0,0 +1,41 @@
+const salary = 4000;
+const lodging = 'apartment';
+const size = 'large';
+
+
+// Only change the syntax below (not the values or key names)
+
+const expenses = {
+    food: 51.7501,
+    transport:  10.2,
+    
+}; // added semicolon
+  
+const tax = {
+    734: '3%',
+    234: '20%',
+    913: '12%',
+    415: '38%',
+    502: '42%',
+};
+
+const rent = {
+    none: 0,
+    'small-room': 200,
+    'large-room': 300,
+    'small-apartment': 400,
+    'large-apartment': 800,
+    'small-house': 1200,
+    'large-house': 2400,
+};
+
+// You can change below however you want
+// converting tax
+const taxAsDecimal = parseInt(tax[913]) / 100;
+// deducting tax from salary
+const startingAfterTax = salary * (1-taxAsDecimal)
+const type = lodging + size
+// balance after expenses are deducting
+const balance = startingAfterTax - expenses.transport - expenses.food - rent['large-apartment'];
+//output the balance
+console.log(balance.toFixed(2));
